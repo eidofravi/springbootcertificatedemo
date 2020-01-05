ac05403c832ac79a1d5dde3a146e03c1b77aab65
eidofravi <ravi.kh123@gmail.com>
2020-01-04 22:48:41 -0500

    Initial commit

---
+++ src/test/java/com/test/controller/SpringbootcertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcertificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
